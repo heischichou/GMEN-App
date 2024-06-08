2d0a20c7e488c7f2c71f341b09f36731bd0b83c3
Heischichou <jangarot@gmail.com>
2024-06-08 18:39:02 +0800

    Implement GraphQL CRUD on MongoDB

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -2,32 +2,16 @@ import {
   GraphQLSchema,
   GraphQLObjectType,
   GraphQLNonNull,
-  // GraphQLID,
   GraphQLString,
   GraphQLInt,
   GraphQLEnumType,
   GraphQLBoolean,
   GraphQLList,
 } from "graphql";
-// import { Employee, Company } from "@/types";
-import { data } from "@/data";
 
 // Mongoose Models
-import CompanyModel from "@/models/Company";
-import EmployeeModel from "@/models/Employee"
-
-const { employees, companies } = data;
-// const EmployeeFields = Object.keys(employees[0]);
-// const CompanyFields = Object.keys(companies[0]);
-
-const CompanyType = new GraphQLObjectType({
-  name: "Company",
-  fields: () => ({
-    id: { type: new GraphQLNonNull(GraphQLString) },
-    name: { type: new GraphQLNonNull(GraphQLString) },
-    geo: { type: new GraphQLNonNull(GraphQLString) },
-  }),
-});
+import Company from "@/models/Company";
+import Employee from "@/models/Employee";
 
 const NullableGenderEnum = new GraphQLEnumType({
   name: "Gender",
@@ -40,69 +24,261 @@ const NullableGenderEnum = new GraphQLEnumType({
 
 const GenderEnum = new GraphQLNonNull(NullableGenderEnum);
 
-const EmployeeType = new GraphQLObjectType({
+const EmployeeStaticFields = (fields: any) => ({
+  id: { type: GraphQLString },
+  name: { type: GraphQLString },
+  position: { type: GraphQLString },
+  gender: { type: NullableGenderEnum },
+  age: { type: GraphQLInt },
+  active: { type: GraphQLBoolean },
+  company_id: { type: GraphQLString },
+  ...fields,
+});
+
+let EmployeeType = new GraphQLObjectType({
   name: "Employee",
+  fields: () => EmployeeStaticFields({}),
+});
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
   fields: () => ({
-    id: { type: new GraphQLNonNull(GraphQLString) },
-    name: { type: new GraphQLNonNull(GraphQLString) },
-    position: { type: new GraphQLNonNull(GraphQLString) },
-    gender: { type: GenderEnum },
-    age: { type: new GraphQLNonNull(GraphQLInt) },
-    active: { type: new GraphQLNonNull(GraphQLBoolean) },
-    company_id: { type: new GraphQLNonNull(GraphQLString) },
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    geo: { type: GraphQLString },
+    employees: {
+      type: new GraphQLList(EmployeeType),
+      resolve: async (root: any) => {
+        try {
+          return await Employee.find({ company_id: root._id });
+        } catch (error: any) {
+          console.log(error);
+          return error;
+        }
+      },
+    },
   }),
 });
+
+EmployeeType = new GraphQLObjectType({
+  name: "Employee",
+  fields: () =>
+    EmployeeStaticFields({
+      company: {
+        type: new GraphQLNonNull(CompanyType),
+        resolve: async (root: any) => {
+          try {
+            return await Company.findById(root.company_id);
+          } catch (error: any) {
+            console.log(error);
+            return error;
+          }
+        },
+      },
+    }),
+});
+
+const GET = {
+  getEmployees: {
+    type: new GraphQLNonNull(new GraphQLList(EmployeeType)),
+    resolve: async (root: any) => {
+      try {
+        return await Employee.find();
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+  getEmployee: {
+    type: new GraphQLNonNull(EmployeeType),
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        return await Employee.findById(args.id).exec();
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+  getCompanies: {
+    type: new GraphQLNonNull(new GraphQLList(CompanyType)),
+    resolve: async (root: any) => {
+      try {
+        return await Company.find();
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+  getCompany: {
+    type: new GraphQLNonNull(CompanyType),
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        return await Company.findById(args.id).exec();
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+};
+
+const POST = {
+  createCompany: {
+    type: CompanyType,
+    args: {
+      name: { type: new GraphQLNonNull(GraphQLString) },
+      geo: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        const company = await Company.create({
+          ...args,
+        });
+
+        console.log("Successfully created a new company.", company);
+        return company;
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+  createEmployee: {
+    type: EmployeeType,
+    args: {
+      name: { type: new GraphQLNonNull(GraphQLString) },
+      position: { type: new GraphQLNonNull(GraphQLString) },
+      gender: { type: GenderEnum },
+      age: { type: new GraphQLNonNull(GraphQLInt) },
+      active: { type: new GraphQLNonNull(GraphQLBoolean) },
+      company_id: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        const employee = await Employee.create({
+          ...args,
+        });
+
+        console.log("Successfully created a new employee.", employee);
+        return employee;
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+};
+
+const PUT = {
+  updateCompany: {
+    type: CompanyType,
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLString) },
+      name: { type: GraphQLString },
+      geo: { type: GraphQLString },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        const { id, ...body } = args;
+        return await Company.findByIdAndUpdate(
+          args.id,
+          {
+            ...body,
+          },
+          {
+            new: true,
+          }
+        ).exec();
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+  updateEmployee: {
+    type: EmployeeType,
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLString) },
+      name: { type: GraphQLString },
+      position: { type: GraphQLString },
+      gender: { type: NullableGenderEnum },
+      age: { type: GraphQLInt },
+      active: { type: GraphQLBoolean },
+      company_id: { type: GraphQLString },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        const { id, ...body } = args;
+        return await Employee.findByIdAndUpdate(
+          args.id,
+          {
+            ...body,
+          },
+          {
+            new: true,
+          }
+        ).exec();
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+};
+
+const DELETE = {
+  deleteEmployee: {
+    type: new GraphQLNonNull(EmployeeType),
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        return await Employee.findByIdAndDelete(args.id);
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+  deleteCompany: {
+    type: new GraphQLNonNull(CompanyType),
+    args: {
+      id: { type: new GraphQLNonNull(GraphQLString) },
+    },
+    resolve: async (root: any, args: any) => {
+      try {
+        await Employee.deleteMany({ company_id: args.id });
+        return await Company.findByIdAndDelete(args.id);
+      } catch (error: any) {
+        console.log(error);
+        return error;
+      }
+    },
+  },
+};
+
+const mutations = {
+  ...GET,
+  ...POST,
+  ...PUT,
+  ...DELETE,
+};
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
     server_status: { type: GraphQLString },
-    employee: {
-      type: new GraphQLNonNull(EmployeeType),
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLString) },
-      },
-      resolve: async (root, { id }) => {
-        try {
-          return employees.find((Employee: any) => Employee.id === id);
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    },
-    employees: {
-      type: new GraphQLNonNull(new GraphQLList(EmployeeType)),
-      resolve: async (root, args) => {
-        try {
-          return EmployeeModel.find();
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    },
-    company: {
-      type: new GraphQLNonNull(CompanyType),
-      args: {
-        id: { type: new GraphQLNonNull(GraphQLString) },
-      },
-      resolve: async (root, { id }) => {
-        try {
-          return companies.find((company: any) => company.id === id);
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    },
-    companies: {
-      type: new GraphQLNonNull(new GraphQLList(CompanyType)),
-      resolve: async (root, args) => {
-        try {
-          return CompanyModel.find();
-        } catch (error) {
-          console.log(error);
-        }
-      },
-    },
+    ...mutations,
   },
 });
 
